9743f16f26393e9d85d170dfc8bbccb79de4df16
nweinberg97 <111965378+nweinberg97@users.noreply.github.com>
2026-09-10 12:17:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -306,7 +306,7 @@ function showColorMenu(event, tab) {
     top: `${event.pageY}px`
   });
 
-boardColors.forEach(color => {
+  boardColors.forEach(color => {
     const swatch = document.createElement('div');
     swatch.className = 'color-swatch';
     swatch.style.backgroundColor = color;
